2096a88427bfa8d218dec7e21bea1f597419df5e
Glen Taggart <glenmtaggart@gmail.com>
2025-11-17 16:29:31 -0800

    move paramsight.py to _paramsight.py

--- src/paramsight/__init__.py
+++ src/paramsight/__init__.py
@@ -1,4 +1,5 @@
+import paramsight._paramsight as _paramsight
+from paramsight._paramsight import get_resolved_typevars_for_base
 from paramsight.aliasclassmethod import takes_alias
-from paramsight.paramsight import get_resolved_typevars_for_base
 
 __all__ = ["takes_alias", "get_resolved_typevars_for_base"]
